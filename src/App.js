5a3e0e464e44c8a11db5d5567917becf6ceb3936
shivani2029 <helloshivi123@gmail.com>
2024-12-02 18:57:26 +0530

    changeupdate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          shivani05 Edit <code>src/App.js</code> and save to reload.
+          shivanikush Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link" 
