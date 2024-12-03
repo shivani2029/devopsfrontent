a6b0623506a4ef926191fcaa375bac48daafaedc
shivani2029 <helloshivi123@gmail.com>
2024-12-03 14:13:20 +0530

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y nodejs npm
-sudo mkdir -p /var/www/react-app
-cd /var/www/react-app || exit
-sudo npm install
 
+# Update package list and install dependencies
+sudo apt update
+
+# Install Node.js and NPM
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs npm
+
+# Create the application directory
+sudo mkdir -p /var/www/react-app
+
+# Change to the app directory
+cd /var/www/react-app || exit
+
+# Install the app dependencies
+sudo npm install
