c41c6ebdfb4884b0932df0780e177d56799f322b
shivani2029 <helloshivi123@gmail.com>
2024-12-03 18:21:22 +0530

    new

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,33 +1,4 @@
 #!/bin/bash
-
-# sudo systemctl start nginx
-
-
-# sudo tee /etc/nginx/sites-available/react-app <<EOL
-# server {
-#     listen 4000;
-#     server_name _;
-#     root /var/www/react-app/build;  # Pointing to the build directory
-#     index index.html;
-
-#     location / {
-#         try_files \$uri /index.html;
-#     }
-# }
-# EOL
-
-
-# sudo ln -sf /etc/nginx/sites-available/react-app /etc/nginx/sites-enabled/
-
-
-# cd /var/www/react-app || exit
-
-#  set -e
-#  npm install
-#  npm run build
-#  PORT=4000 npm start &
-
-#!/bin/bash
 cd /home/ubuntu/react-app
 npm run build
 npm start
@@ -36,4 +7,4 @@ npm start
 
 
 
-# sudo systemctl restart nginx
+
