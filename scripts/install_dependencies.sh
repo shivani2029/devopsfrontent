db296b5cd35507e8315ae275ab2633aa0edc4d8e
shivani2029 <helloshivi123@gmail.com>
2024-12-04 12:11:28 +0530

    newg

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 sudo apt update
-# sudo apt install -y nodejs npm
-set -x # Enables debug logging
-
-echo "Checking for Node.js version..."
-node -v
-
-echo "Checking for npm version..."
-npm -v
-
+sudo apt install -y nodejs npm
 cd /home/ubuntu/react-app
 npm install
 
